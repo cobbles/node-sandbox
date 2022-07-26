d813796d70c08bc3100e785201778e48de4cd691
Tom Molloy <cobbles@molloy.cloud>
2022-07-27 00:15:28 +1000

    clean up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
 
 export const App = (): JSX.Element => {
-    return <h1>Hello World!</h1>
+    return <h1>Hello World</h1>
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  mode: 'production',
+  mode: isDevelopment ? 'development' : 'production',
   entry: path.resolve(__dirname, "src/index.tsx"),
+  output: {
+    filename: '[name].[contenthash].bundle.js',
+    chunkFilename: '[contenthash].bundle.js',
+  },
   resolve: {
     extensions: [
       '.tsx',
@@ -37,12 +43,34 @@ module.exports = {
     }),
   ],
   optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
     splitChunks: {
-      chunks: 'all',
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
   },
+  performance: {
+    hints: isDevelopment ? false : "warning"
+  },
   devServer: {
-    port: 9000,
-    open: true,
+    hot: true,
   },
 };
